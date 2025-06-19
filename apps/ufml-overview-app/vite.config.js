7e7a22e741e0716cf83a6bfc57cc9669d27392c7
Ikeda Kazuki <40589834+jacknocode@users.noreply.github.com>
2025-06-19 12:55:07 +0900

    Updated vite.config.js

--- apps/ufml-overview-app/vite.config.js
+++ apps/ufml-overview-app/vite.config.js
@@ -2,21 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
   server: {
-    host: '0.0.0.0',
     port: 3001,
-    open: false,
-    strictPort: true
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3001,
-    open: false,
-    strictPort: true
+    open: true
   }
 })
